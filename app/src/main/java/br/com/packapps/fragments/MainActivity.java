41e17d8826761a8bdf297268fc65e0e4447b7029
Paulo Linhares <plinhares9@gmail.com>
2017-09-07 21:32:21 -0300

    with tree fragments

--- app/src/main/java/br/com/packapps/fragments/MainActivity.java
+++ app/src/main/java/br/com/packapps/fragments/MainActivity.java
@@ -3,13 +3,37 @@ package br.com.packapps.fragments;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import br.com.packapps.fragments.fragments.MyFragmentA;
+import br.com.packapps.fragments.fragments.MyFragmentB;
+import br.com.packapps.fragments.fragments.MyFragmentC;
 
-    private TextView mTextMessage;
+public class MainActivity extends AppCompatActivity {
+    private FrameLayout flContainerForFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        flContainerForFragment = (FrameLayout) findViewById(R.id.flContainerForFragment);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        //implementing fragment
+        MyFragmentA fragmentA = new MyFragmentA();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.flContainerForFragment, fragmentA, "Fragment_A");
+        fragmentTransaction.commit();
+    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,28 +42,25 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
+                    MyFragmentA fragmentA = new MyFragmentA();
+                    managerFragment(fragmentA, "Fragment_A");return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    MyFragmentB fragmentB = new MyFragmentB();
+                    managerFragment(fragmentB, "Fragment_B");
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    MyFragmentC fragmentC = new MyFragmentC();
+                    managerFragment(fragmentC, "Fragment_C");
                     return true;
             }
             return false;
         }
-
     };
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    private void managerFragment(Fragment fragment, String tag) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.flContainerForFragment, fragment, tag);
+        fragmentTransaction.commit();
     }
-
 }
---
+++ app/src/main/java/br/com/packapps/fragments/fragments/MyFragmentB.java
@@ -0,0 +1,26 @@
+package br.com.packapps.fragments.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.com.packapps.fragments.R;
+
+public class MyFragmentB extends Fragment {
+
+
+    public MyFragmentB() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_b, container, false);
+    }
+
+}
